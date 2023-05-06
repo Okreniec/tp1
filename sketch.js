32e78cea11d52d89e76581ea27ddbd8ab46dfc7a
Okreniec <ivanokreniec@gmail.com>
2023-05-06 10:03:42 -0300

    clase forma separada

--- sketch.js
+++ sketch.js
@@ -1,27 +1,3 @@
-class Forma {
-  constructor(diametro, y) {
-    this.diametro = diametro;
-    this.y = y;
-  }
-
-  display() {
-    strokeWeight(115);
-    strokeCap(SQUARE);
-  
-    if (this.y < height/2) {
-      stroke(50);
-      line(10, this.y + 30, width - 10, this.y + 30); // Linea de Arriba
-    } else {
-      stroke(50);
-      line(10, this.y - 20, width - 10, this.y - 20); // Linea de Abajo
-    }
-
-    stroke(100);
-    noFill();
-    ellipse(width/2, this.y, this.diametro, this.diametro);
-  }
-}
-
 let semicirculo = [];
 
 function setup() {
