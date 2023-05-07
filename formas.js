2ebdd90a92d9f42cbda129843cbecb8ebe24826d
Okreniec <ivanokreniec@gmail.com>
2023-05-07 11:59:12 -0300

    Formas definidas divididas

--- formas.js
+++ formas.js
@@ -2,45 +2,62 @@ function rectangulo(posicion) {
   strokeWeight(width/8.7); //anchura de las líneas
   strokeCap(SQUARE); //puntas de las líneas (cuadradas)
 
-  if (posicion === 'arriba') { // listo
-
+  if (posicion === 'arribaIzq') { //listo
     stroke(50);
-    line(width - width * 0.99, 0 + height * 0.03, width - 7, 0 + height * 0.03);
+    line(width - width * 0.99, 0 + height * 0.03, width / 2, 0 + height * 0.03);
 
-  } else if (posicion === 'abajo') { //listo
-
+  }  else if (posicion === 'arribaDer'){ //listo
     stroke(50);
-    line(width - width * 0.99, height - height * 0.02, width - 7, height - height * 0.02);
+    line(width / 2.01, 0 + height * 0.03, width - 7, 0 + height * 0.03);
 
-  } else if (posicion === 'derecha') { //listo
-
+  } else if (posicion === 'abajoIzq') { 
     stroke(50);
-    line(width - width * 0.943, 0, width - width * 0.943, height);
+    line(width - width * 0.99, height - height * 0.02, width / 2, height - height * 0.02);
 
-  } else if (posicion === 'izquierda') { //listo
-
+  } else if (posicion === 'abajoDer') { //listo
     stroke(50);
-    line(width - width * 0.056, 0, width - width * 0.056, height);
+    line(width / 2.01, height - height * 0.02, width, height - height * 0.02);
 
+  } else if (posicion === 'derechaSup') { //listo
+    stroke(50);
+    line(width - width * 0.943, 0, width - width * 0.943, height / 2);
+
+  } else if (posicion === 'derechaInf') { //listo
+    stroke(50);
+    line(width - width * 0.943, height / 2.01, width - width * 0.943, height);
+
+  } else if (posicion === 'izquierdaSup') { //listo
+    stroke(50);
+    line(width - width * 0.056, 0, width - width * 0.056, height / 2);
+
+
+  } else if (posicion === 'izquierdaInf') { //listo
+    stroke(50);
+    line(width - width * 0.056, height / 2.01, width - width * 0.056, height);
+  
   }
 }
 
-function circulo(posicion) {
+function circulo(posicion) { //semicirculos terminados
   if (posicion === 'arribaDer') {
     stroke(100);
     noFill();
-    arc(width / 2, 0, width * 0.885, height * 0.885, -PI / 2, PI / 2); 
+    arc(width / 2, 0, width * 0.885, height * 0.885, -PI / 2, PI / 2);
+
   } else if (posicion === 'arribaIzq') {
     stroke(100);
     noFill();
     arc(width / 2, 0, width * 0.885, height * 0.885, PI / 2.1, -PI / 2);
+
   } else if (posicion === 'abajoDer') {
     stroke(100);
     noFill();
     arc(width / 2, height, width * 0.885, height * 0.885, -PI/2, PI / 2);
+
   } else if (posicion === 'abajoIzq') {
     stroke(100);
     noFill();
     arc(width/2, height, width * 0.885, height * 0.885, PI / 2, -PI / 2.1);
+
   }
 }
--- sketch.js
+++ sketch.js
@@ -7,10 +7,15 @@ function draw() {
   background(200);
 
   circulo('arribaDer');
-  rectangulo('arriba'); //linea que está a mitad del circulo de arriba
+  rectangulo('arribaIzq'); //linea que está a mitad del circulo de arriba izquierda
+  rectangulo('arribaDer');
   circulo('arribaIzq');
-  rectangulo('derecha'); //linea a la derecha
-  rectangulo('izquierda'); //linea a la izquierda
+  rectangulo('derechaSup'); //linea a la derecha Arriba
+  rectangulo('derechaInf'); //linea a la derecha Arriba
+  rectangulo('abajoDer');
+  rectangulo('abajoIzq');
+  rectangulo('izquierdaSup'); //linea a la izquierda Arriba
+  rectangulo('izquierdaInf'); //linea a la izquierda Abajo
   
   circulo('abajoIzq');
   circulo('abajoDer');
