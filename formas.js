55bf3b48b0d7419b40fedc7cb41cf9d47a4d4acf
Okreniec <ivanokreniec@gmail.com>
2023-05-07 01:05:36 -0300

    lineas definidas, creo

--- formas.js
+++ formas.js
@@ -1,30 +1,42 @@
 function rectangulo(posicion) {
-  strokeWeight(115);
-  strokeCap(SQUARE);
+  strokeWeight(115); //anchura de las líneas
+  strokeCap(SQUARE); //puntas de las líneas (cuadradas)
 
-  if (posicion === 'arriba') {
+  if (posicion === 'arriba') { // listo
+
     stroke(50);
-    line(width - width/0.2, 0, width - 7, 0); // Linea de Arriba
-  } else if (posicion === 'abajo') {
+    line(width - width * 0.99, 0 + height * 0.03, width - 7, 0 + height * 0.03);
+
+  } else if (posicion === 'abajo') { //listo
+
     stroke(50);
-    line(width - width/0.99, height - 20, width - 7, height - 20); // Linea de Abajo
-  } else if (posicion === 'derecha') {
+    line(width - width * 0.99, height - height * 0.02, width - 7, height - height * 0.02);
+
+  } else if (posicion === 'derecha') { //listo
+
     stroke(50);
-    line(height - 57, 0, height - 57, width); // Linea de Abajo
-  } else if (posicion === 'izquierda') {
+    line(width - width * 0.943, 0, width - width * 0.943, height);
+
+  } else if (posicion === 'izquierda') { //listo
+
     stroke(50);
-    line(57, 0, 57, width); // Linea de Abajo
+    line(width - width * 0.056, 0, width - width * 0.056, height);
+
   }
 }
 
 function circulo(posicion) {
   if (posicion === 'arriba') {
+
     stroke(100);
     noFill();
-    ellipse(width / 2, 0, width* 0.885, height* 0.885);
+    ellipse(width / 2, 0, width * 0.885, height * 0.885);
+
   } else if (posicion === 'abajo') {
+
     stroke(100);
     noFill();
-    ellipse(width / 2, height, width * 0.885, height* 0.885);
+    ellipse(width / 2, height, width * 0.885, height * 0.885);
+
   }
 }
