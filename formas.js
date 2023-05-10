148315b4f865c68568dea5ba01eb150152deebb0
Okreniec <ivanokreniec@gmail.com>
2023-05-10 10:42:56 -0300

    lineas dependientes del mouseX

--- formas.js
+++ formas.js
@@ -1,5 +1,13 @@
+let Ancho;
+let AnchoLineas;
+
 function rectangulo(posicion) { //Rectangulos terminados
-  strokeWeight(width/8.7); //anchura de las líneas
+
+  Ancho = map(mouseX, 0, width, 13, 11);
+
+  AnchoLineas = map(mouseX, 0, width, 15, 12.6);
+
+  strokeWeight(width/Ancho);
   strokeCap(SQUARE); //puntas de las líneas (cuadradas)
 
   if (posicion === 'arribaIzq') { //listo
@@ -7,30 +15,30 @@ function rectangulo(posicion) { //Rectangulos terminados
     fill("#81eaa8");
     quad(0, 0,
     width / 2, 0,
-    width / 2, height / 11.4,
-    width / 8.8, height / 11.4);
+    width / 2, height / AnchoLineas,
+    width / AnchoLineas, height / AnchoLineas );
 
   }  else if (posicion === 'arribaDer'){ //listo
     noStroke();
     fill("#b11e31");
     quad(width / 2.01, 0,
     width, 0,
-    width - width / 8.8, height / 11.4,
-    width / 2.01, height / 11.4);
+    width - width / AnchoLineas, height / AnchoLineas,
+    width / 2.01, height / AnchoLineas);
 
   } else if (posicion === 'abajoIzq') { //listo
     noStroke();
     fill("#060411");
-    quad(width / 8.8, height - height / 11.4,
-    width / 2, height - height / 11.4,
+    quad(width / AnchoLineas, height - height / AnchoLineas,
+    width / 2, height - height / AnchoLineas,
     width / 2, height,
     0, height);
 
   } else if (posicion === 'abajoDer') { //listo
     noStroke();
     fill("#dd98d7");
-    quad(width / 2.01, height - height / 11.4,
-    width - width / 8.8, height - height / 11.4,
+    quad(width / 2.01, height - height / AnchoLineas,
+    width - width / AnchoLineas, height - height / AnchoLineas,
     width, height,
     width / 2.01, height);
 
@@ -39,14 +47,14 @@ function rectangulo(posicion) { //Rectangulos terminados
     fill("#b11e31");
     quad(width, height / 2,
     width, -1,
-    width - width / 8.8, height / 11.5,
-    width - width / 8.8, height / 2);
+    width - width / AnchoLineas, height / AnchoLineas,
+    width - width / AnchoLineas, height / 2);
 
   } else if (posicion === 'derechaInf') { //listo
     noStroke();
     fill("#b11e31");
-    quad(width - width / 8.8, height / 2.01,
-    width - width / 8.8, height - height / 11.5,
+    quad(width - width / AnchoLineas, height / 2.01,
+    width - width / AnchoLineas, height - height / AnchoLineas,
     width, height+1,
     width, height / 2.01);
 
@@ -55,14 +63,14 @@ function rectangulo(posicion) { //Rectangulos terminados
     fill("#060411");
     quad(0, -1,
     0, height / 2,
-    width / 8.8, height / 2,
-    width / 8.8, height / 11.5);
+    width / AnchoLineas, height / 2,
+    width / AnchoLineas, height / AnchoLineas);
 
   } else if (posicion === 'izquierdaInf') { //listo
     noStroke();
     fill("#060411");
-    quad(width / 8.8, height - height / 11.5,
-    width / 8.8, height / 2.01,
+    quad(width / AnchoLineas, height - height / AnchoLineas,
+    width / AnchoLineas, height / 2.01,
     0, height / 2.01,
     0, height+1);
   
@@ -73,22 +81,22 @@ function circulo(posicion) { //semicirculos terminados
   if (posicion === 'arribaDer') {
     stroke("#81eaa8");
     noFill();
-    arc(width / 2, 0, width - width / 8.7 , height - height / 8.7, -PI / 2, PI / 2);
+    arc(width / 2, 0, width - width / Ancho , height - height / Ancho, -PI / 2, PI / 2);
 
   } else if (posicion === 'arribaIzq') {
     stroke("#81eaa8");
     noFill();
-    arc(width / 2, 0, width - width / 8.7 , height - height / 8.7, PI / 2.005, -PI / 2);
+    arc(width / 2, 0, width - width / Ancho , height - height / Ancho, PI / 2.005, -PI / 2);
 
   } else if (posicion === 'abajoDer') {
     stroke("#dd98d7");
     noFill();
-    arc(width / 2, height, width - width / 8.7 , height - height / 8.7, -PI / 2, PI / 2);
+    arc(width / 2, height, width - width / Ancho , height - height / Ancho, -PI / 2, PI / 2);
 
   } else if (posicion === 'abajoIzq') {
     stroke("#dd98d7");
     noFill();
-    arc(width / 2, height, width - width / 8.7 , height - height / 8.7, PI / 2, -PI / 2.005);
+    arc(width / 2, height, width - width / Ancho , height - height / Ancho, PI / 2, -PI / 2.005);
 
   }
 }
