9a8d817dbd08e6be9483ed25879efee1e3c9377b
Okreniec <ivanokreniec@gmail.com>
2023-05-10 14:26:45 -0300

    terminado

--- sketch.js
+++ sketch.js
@@ -5,7 +5,6 @@ let colores3 = ["#ffbf00", "#00aedb", "#81eaa8", "#dd98d7", "#b11e31"];
 let colores4 = ["#00aedb", "#81eaa8", "#dd98d7", "#b11e31", "#ffbf00", "#060411"];
 //let colores5 = ["#0a9777", "#5450bf", "#0998c0", "#03476a"];
 
-let opacidad=150;
 
 
 function setup() {
@@ -13,13 +12,13 @@ function setup() {
 }
 
 function draw() {
- 
-  indexColor = floor(map(mouseY, 0, height, 0, colores.length));
-  color0 = color(colores[indexColor] + hex(opacidad, 2));
-  color1 = color(colores[indexColor] + hex(opacidad, 2));
-  color2 = color(colores[indexColor] + hex(opacidad, 2));
-  color3 = color(colores[indexColor] + hex(opacidad, 2));
-  color4 = color(colores[indexColor] + hex(opacidad, 2));
+
+  let indexColor = floor(map(mouseY, 0, height, 0, colores.length));
+  color0 = colores[indexColor];
+  color1 = colores1[indexColor];
+  color2 = colores2[indexColor];
+  color3 = colores3[indexColor];
+  color4 = colores[indexColor];
   //color5 = colores5[indexColor];
 
   background('#0a9777');
@@ -50,4 +49,10 @@ function draw() {
   circulo('bordeInfIzq');
   circulo('bordeInfDer');
   
+}
+
+function mouseMoved() {
+  // Actualizar el tiempo y la posición del mouse
+  lastTimestamp = millis();
+  lastMouseY = mouseY;
 }
