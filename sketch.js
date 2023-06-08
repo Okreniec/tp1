c456d49d1cd17c023bea99e4078c17e2040940e5
Okreniec <ivanokreniec@gmail.com>
2023-06-08 11:54:02 -0300

    velocidad mouse alpha

--- sketch.js
+++ sketch.js
@@ -5,14 +5,22 @@ let colores3 = ["#ffbf00", "#00aedb", "#81eaa8", "#dd98d7", "#b11e31"];
 let colores4 = ["#00aedb", "#81eaa8", "#dd98d7", "#b11e31", "#ffbf00", "#060411"];
 //let colores5 = ["#0a9777", "#5450bf", "#0998c0", "#03476a"];
 
-
+let prevX, prevY;
+let prevTime;
+let speed;
+let drawCircle = false;
 
 function setup() {
   createCanvas(windowHeight, windowHeight);
+  prevX = mouseX;
+  prevY = mouseY;
+  prevTime = millis();
 }
 
 function draw() {
-
+  
+  Velocidad();
+  
   let indexColor = floor(map(mouseY, 0, height, 0, colores.length));
   color0 = colores[indexColor];
   color1 = colores1[indexColor];
@@ -48,4 +56,31 @@ function draw() {
   circulo('bordeSupIzq');
   circulo('bordeInfIzq');
   circulo('bordeInfDer');
+
+  if (drawCircle) {
+    fill(colores2);
+    noStroke();
+    blendMode(DIFFERENCE);
+    circle(width / 2, height / 2, width / 12 + width / AnchoLineas, height / 5 + width / AnchoLineas);
+    blendMode(BLEND);
+  }
+}
+
+function Velocidad() {
+  let distX = mouseX - prevX;
+  let distY = mouseY - prevY;
+  let distance = sqrt(distX * distX + distY * distY);
+
+  let currentTime = millis();
+  let deltaTime = currentTime - prevTime;
+
+  speed = distance / deltaTime;
+
+  prevX = mouseX;
+  prevY = mouseY;
+  prevTime = currentTime;
+
+  if (speed >= 1 && speed <= 2) {
+    drawCircle = true; // Activar el dibujado del círculo
+  }
 }
